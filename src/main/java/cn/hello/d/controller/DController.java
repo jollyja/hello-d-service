ef26e0669358fe34a2092020cd9c63ae48cb3336
王培嘉 <jollyja@gmail.com>
2021-03-17 11:33:36 +0800

    test

---
+++ src/main/java/cn/hello/d/controller/DController.java
@@ -0,0 +1,28 @@
+package cn.hello.d.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class DController {
+
+    @PostMapping(path = "/d/done")
+    public Map<String, String> done(@RequestBody Map<String, String> req) {
+        Map<String, String> res = new HashMap<String, String>();
+        res.put("abc", "def");
+        return res;
+    }
+
+
+//    @PostMapping(path = "/entrance")
+//    @ApiOperation(value = "风控实时决策入口", notes = "计算和执行风控规则")
+//    public RiskResponse entrance(@RequestBody Map<String, Object> riskRequest) {
+//        return riskEntranceService.execute(riskRequest);
+//    }
+
+
+}
---
+++ src/test/java/cn/hello/d/DApplicationTests.java
@@ -0,0 +1,14 @@
+package cn.hello.d;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("hello d world");
+    }
+
+}
