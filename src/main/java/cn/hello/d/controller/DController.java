e2fd3ef50058423a33e9bfae10ee14209f315e18
王培嘉 <jollyja@gmail.com>
2021-05-01 23:06:17 +0800

    nacos

--- src/main/java/cn/hello/d/controller/DController.java
+++ src/main/java/cn/hello/d/controller/DController.java
@@ -1,25 +1,23 @@
 package cn.hello.d.controller;
 
-import com.alibaba.nacos.api.annotation.NacosInjected;
-import com.alibaba.nacos.api.config.annotation.NacosValue;
 import com.alibaba.nacos.api.exception.NacosException;
-import com.alibaba.nacos.api.naming.NamingService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@Slf4j
+@RefreshScope
 public class DController {
 
-    @NacosValue(value = "${whoami:unkonwn}", autoRefreshed = true)
+    @Value("${whoami:unkonwn}")
     private String whoami;
-
-    @NacosInjected
-    private NamingService namingService;
 
     @PostMapping(path = "/d/done")
     public Map<String, String> done(@RequestBody Map<String, String> req) throws NacosException {
@@ -27,10 +25,7 @@ public class DController {
         res.put("abc", "def");
         res.put("whoami", whoami);
         System.out.println("who am I : " + whoami);
-        System.out.println(namingService.getAllInstances("hello_d", "helloGroup"));
+
         return res;
     }
-
-
-
 }
